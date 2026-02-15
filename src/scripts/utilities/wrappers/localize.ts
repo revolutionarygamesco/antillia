082df105f91a466553e3d958c4701bce0265b003
Jason Godesky <jefgodesky@gmail.com>
2026-02-15 16:20:42 -0500

    localize: Return key, even if given array

--- src/scripts/utilities/wrappers/localize.ts
+++ src/scripts/utilities/wrappers/localize.ts
@@ -3,7 +3,7 @@ const localize = (
   options?: Record<string, any>
 ) => {
   const k = typeof key === 'string' ? key : key.join('.')
-  return game?.i18n?.format?.(k, options) ?? key
+  return game?.i18n?.format?.(k, options) ?? k
 }
 
 export default localize
