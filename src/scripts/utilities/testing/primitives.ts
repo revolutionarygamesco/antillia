55a10055bc68969493815e3321111b40534eabe2
Jason Godesky <jefgodesky@gmail.com>
2026-02-16 13:28:03 -0500

    Add testing utility for primitives to test

---
+++ src/scripts/utilities/testing/primitives.ts
@@ -0,0 +1,17 @@
+export const primitives = [
+  ['null', null],
+  ['undefined', undefined],
+  ['a function', () => {}],
+  ['true', true],
+  ['false', false],
+  ['a string', 'a string'],
+  ['a number', 42],
+  ['an array', []],
+  ['an empty object', {}],
+] as Array<[string, any]>
+
+export const getPrimitivesExcept = (...exceptions: string[]): Array<[string, any]> => {
+  return primitives.filter(([label, _]) => {
+    return !exceptions.includes(label)
+  })
+}
