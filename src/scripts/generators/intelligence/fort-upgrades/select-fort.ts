061fa9c00e09fd424a68c217e9ab0a4aa754f7f7
Jason Godesky <jefgodesky@gmail.com>
2026-02-16 20:10:30 -0500

    Centralize UUIDs in settings

--- src/scripts/generators/intelligence/fort-upgrades/select-fort.ts
+++ src/scripts/generators/intelligence/fort-upgrades/select-fort.ts
@@ -1,10 +1,9 @@
+import { UUIDS } from '../../../settings.ts'
 import fromUuid from '../../../utilities/wrappers/from-uuid.ts'
 import makeLink from '../../../utilities/make-link.ts'
 
-const FORTS = 'RollTable.XleTdybFWP2znvEq'
-
 const selectFort = async (): Promise<string> => {
-  const table = await fromUuid(FORTS) as RollTable
+  const table = await fromUuid(UUIDS.FORTS) as RollTable
   if (!table.draw) return ''
   const drawn = await table.draw({ displayChat: false }) satisfies { results: TableResult[] }
   return makeLink(drawn.results[0])
--- src/scripts/settings.ts
+++ src/scripts/settings.ts
@@ -1,2 +1,7 @@
 export const MODULE_NAME = 'Shadows of Antillia'
 export const MODULE_ID = 'revolutionary-antillia'
+export const UUIDS: Record<string, string> = {
+  LOG: 'JournalEntry.bhjmsYTe3uIoHyd9',
+  TIMELINE: 'JournalEntry.bhjmsYTe3uIoHyd9.JournalEntryPage.ONIKQ49Szsbcy0J3',
+  FORTS: 'RollTable.XleTdybFWP2znvEq'
+}
