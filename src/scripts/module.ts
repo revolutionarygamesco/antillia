347a314129fe265371ec58d3594610e376f80f03
Jason Godesky <jefgodesky@gmail.com>
2026-02-16 20:24:32 -0500

    Save adventure on load

--- src/scripts/module.ts
+++ src/scripts/module.ts
@@ -1,3 +1,4 @@
+import AdventureState from './state/adventure/class.ts'
 import { MODULE_NAME } from './settings.ts'
 
 Hooks.on('init', () => {
@@ -6,4 +7,9 @@ Hooks.on('init', () => {
 
 Hooks.on('ready', () => {
   console.log(`${MODULE_NAME} is ready`)
+})
+
+Hooks.once('ready', async () => {
+  const state = await AdventureState.load()
+  await state.save()
 })
