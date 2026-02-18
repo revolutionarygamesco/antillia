df09caab49dc2da595e3da3a6019cf899cff732a
Jason Godesky <jefgodesky@gmail.com>
2026-02-18 18:39:11 -0500

    selectRandomElement: Off-by-1 error

--- src/scripts/random/el.ts
+++ src/scripts/random/el.ts
@@ -1,7 +1,7 @@
 import selectRandomBetween from './between.ts'
 
 const selectRandomElement = <T>(arr: T[]): T => {
-  const index = selectRandomBetween(1, arr.length - 1)
+  const index = selectRandomBetween(0, arr.length - 1)
   return arr[index]
 }
 
