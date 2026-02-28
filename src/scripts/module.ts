6900011b9a3e490630cf4ba3415acf2a63a4700d
Jason Godesky <jefgodesky@gmail.com>
2026-02-28 18:46:23 -0500

    Add /log chat command

--- src/scripts/module.ts
+++ src/scripts/module.ts
@@ -1,4 +1,6 @@
 import AdventureState from './state/adventure/class.ts'
+import localize from './utilities/wrappers/localize.ts'
+import addLog from './log/add.ts'
 import initLog from './log/init.ts'
 import { MODULE_ID } from './settings.ts'
 
@@ -19,4 +21,29 @@ Hooks.once('ready', async () => {
   await initLog()
   const state = await AdventureState.load()
   await state.save()
+})
+
+Hooks.once('chatCommandsReady', commands => {
+  const prefix = [MODULE_ID, 'log', 'command']
+
+  commands.register({
+    name: '/log',
+    module: 'revolutionary-antillia',
+    description: localize([...prefix, 'description']),
+    icon: '<i class="fa-solid fa-book"></i>',
+    callback: (_chat: any, parameters: any) => {
+      addLog(parameters).then()
+      return {}
+    },
+    autocompleteCallback: (menu: any, alias: any) => {
+      const entries = [
+        (game as any).chatCommands.createCommandElement(
+          localize([...prefix, 'autocomplete', 'alias'], { alias }),
+          localize([...prefix, 'autocomplete', 'description']),
+        )
+      ]
+      entries.length = Math.min(entries.length, menu.maxEntries)
+      return entries
+    }
+  })
 })
