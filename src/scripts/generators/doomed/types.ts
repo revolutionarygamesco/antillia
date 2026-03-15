709e795ae24d15dd3fd9645f0c3b516a008490dd
Jason Godesky <jefgodesky@gmail.com>
2026-03-15 19:36:32 -0400

    Add types to construct doomed message

--- src/scripts/generators/doomed/types.ts
+++ src/scripts/generators/doomed/types.ts
@@ -2,3 +2,22 @@ export interface DoomedSituation {
   tag: string
   complications: string[]
 }
+
+export interface DoomedKin {
+  name: string
+  relationship: string
+  location: JournalEntry | string
+}
+
+export interface DoomedShip {
+  ship: Actor
+  captain: Actor
+  author: {
+    name: string
+    position: string
+  }
+  tale: string
+  complication: string
+  includeMap: boolean
+  kin: DoomedKin[]
+}
