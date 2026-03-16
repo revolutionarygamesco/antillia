ea4e9bf50c7b6185154fee2ae41ae3b275778194
Jason Godesky <jefgodesky@gmail.com>
2026-03-16 17:42:11 -0400

    Add new method: getOldCountry

---
+++ src/scripts/generators/doomed/old-country.ts
@@ -0,0 +1,13 @@
+import localize from '../../utilities/wrappers/localize.ts'
+import { MODULE_ID } from '../../settings.ts'
+
+const getOldCountry = (): Record<string, string> => {
+  const nationalities = ['dutch', 'english', 'french', 'irish', 'scottish', 'spanish', 'welsh']
+  const country: Record<string, string> = {}
+  for (const nationality of nationalities) {
+    country[nationality] = localize([MODULE_ID, 'last-accounts', 'item', 'requests', 'old-country', nationality])
+  }
+  return country
+}
+
+export default getOldCountry
