b7e02932b07a4915d33ee53d18d37e9ddb721387
Jason Godesky <jefgodesky@gmail.com>
2026-03-15 20:58:00 -0400

    Add new method: addSacrificeContext

---
+++ src/scripts/generators/doomed/context/sacrifice.ts
@@ -0,0 +1,28 @@
+import type { DoomedContextAdder, DoomedShip } from '../types.ts'
+import localize from '../../../utilities/wrappers/localize.ts'
+import selectRandomElement from '../../../random/el.ts'
+import stockArray from '../../../random/stock.ts'
+import { MODULE_ID } from '../../../settings.ts'
+
+const addSacrificeContext = (
+  context: Record<string, string>
+): void => {
+  const tag = selectRandomElement(stockArray([
+    { n: 21, item: 'devil' },
+    { n: 7, item: 'davy' },
+    { n: 2, item: 'thor' },
+    { n: 1, item: 'aegir' },
+    { n: 2, item: 'odin' },
+    { n: 1, item: 'njordr' },
+    { n: 3, item: 'neptune' },
+    { n: 3, item: 'poseidon' },
+    { n: 2, item: 'manannan' }
+  ]))
+
+  context.god = localize([MODULE_ID, 'last-accounts', 'item', 'tales', 'sacrifice', 'gods', tag])
+}
+
+export default {
+  test: (scenario: DoomedShip) => scenario.tale === 'sacrifice',
+  adder: addSacrificeContext
+} as DoomedContextAdder
