41bc1de38b2c0e658db975d7576edc7df46efbad
Jason Godesky <jefgodesky@gmail.com>
2026-03-15 21:21:55 -0400

    Add new method: addContext

---
+++ src/scripts/generators/doomed/context/add.ts
@@ -0,0 +1,25 @@
+import type { DoomedShip, DoomedContextAdder } from '../types.ts'
+import battle from './battle.ts'
+import carrion from './carrion.ts'
+import davy from './davy.ts'
+import disease from './disease.ts'
+import haunted from './haunted.ts'
+import kraken from './kraken.ts'
+import orcas from './orcas.ts'
+import sacrifice from './sacrifice.ts'
+import whale from './whale.ts'
+
+const addContext = async (
+  scenario: DoomedShip,
+  context: Record<string, string>,
+  isPremium: boolean = false
+): Promise<void> => {
+  const adders: DoomedContextAdder[] = [battle, carrion, davy, disease,
+    haunted, kraken, orcas, sacrifice, whale]
+
+  for await (const { test, adder } of adders) {
+    if (test(scenario)) await adder(context, isPremium)
+  }
+}
+
+export default addContext
