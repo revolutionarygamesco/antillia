a2b7c1d798ba510157d8568a9435a48e49b76a35
Jason Godesky <jefgodesky@gmail.com>
2026-03-15 20:33:44 -0400

    Add new method: addDavyJonesContext

---
+++ src/scripts/generators/doomed/haunted/davy.ts
@@ -0,0 +1,18 @@
+import type { DoomedContextAdder, DoomedShip } from '../types.ts'
+import localize from '../../../utilities/wrappers/localize.ts'
+import makeLink from '../../../utilities/make-link.ts'
+import { MODULE_ID, UUIDS } from '../../../settings.ts'
+
+const addDavyJonesContext = (
+  context: Record<string, string>,
+  isPremium: boolean = false
+): void => {
+  const name = localize([MODULE_ID, 'last-accounts', 'item', 'tales', 'davy', 'link'])
+  const uuid = UUIDS.PREMIUM_DAVY_JONES
+  context.davy = isPremium ? makeLink({ name, uuid }) : name
+}
+
+export default {
+  test: (scenario: DoomedShip) => scenario.tale === 'davy',
+  adder: addDavyJonesContext
+} as DoomedContextAdder
