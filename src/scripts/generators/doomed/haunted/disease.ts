bd02d6a21b603c0abd78c6722fa4ce9565eec967
Jason Godesky <jefgodesky@gmail.com>
2026-03-15 20:52:38 -0400

    Add new method: addDiseaseContext

---
+++ src/scripts/generators/doomed/haunted/disease.ts
@@ -0,0 +1,20 @@
+import type { DoomedContextAdder, DoomedShip } from '../types.ts'
+import { pickRandomOutbreakDisease } from '../../intelligence/outbreak/diseases.ts'
+import capitalize from '../../../utilities/capital.ts'
+import localize from '../../../utilities/wrappers/localize.ts'
+import makeLink from '../../../utilities/make-link.ts'
+import { MODULE_ID } from '../../../settings.ts'
+
+const addDiseaseContext = (
+  context: Record<string, string>
+): void => {
+  const disease = pickRandomOutbreakDisease()
+  const name = capitalize(localize([MODULE_ID, 'intelligence', 'outbreak', 'diseases', disease.tag, 'sentence']))
+  const uuid = disease.uuid
+  context.kraken = makeLink({ name, uuid })
+}
+
+export default {
+  test: (scenario: DoomedShip) => scenario.tale === 'disease',
+  adder: addDiseaseContext
+} as DoomedContextAdder
