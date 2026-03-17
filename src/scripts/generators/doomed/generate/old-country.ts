79d1fa7b69050a12caea0bf914c4ff162f3bdef2
Jason Godesky <jefgodesky@gmail.com>
2026-03-16 22:06:19 -0400

    getOldCountry: Properly localize the name of the old country

--- src/scripts/generators/doomed/generate/old-country.ts
+++ src/scripts/generators/doomed/generate/old-country.ts
@@ -2,10 +2,11 @@ import localize from '../../../utilities/wrappers/localize.ts'
 import { MODULE_ID } from '../../../settings.ts'
 
 const getOldCountry = (): Record<string, string> => {
+  const prefix = [MODULE_ID, 'last-accounts', 'item', 'request', 'relationships', 'old-country']
   const nationalities = ['dutch', 'english', 'french', 'irish', 'scottish', 'spanish', 'welsh']
   const country: Record<string, string> = {}
   for (const nationality of nationalities) {
-    country[nationality] = localize([MODULE_ID, 'last-accounts', 'item', 'requests', 'old-country', nationality])
+    country[nationality] = localize([...prefix, nationality])
   }
   return country
 }
