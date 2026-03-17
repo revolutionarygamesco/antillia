d51d3ca174f22b63a0b53d9f46295a0989a2540f
Jason Godesky <jefgodesky@gmail.com>
2026-03-16 22:07:44 -0400

    getKin: Use first names for relatives, fix old country lookup

--- src/scripts/generators/doomed/generate/kin.ts
+++ src/scripts/generators/doomed/generate/kin.ts
@@ -5,6 +5,7 @@ import generatePersonalName from '../../../utilities/name.personal.ts'
 import getOldCountry from './old-country.ts'
 import selectRandomElement from '../../../random/el.ts'
 import stockArray from '../../../random/stock.ts'
+import getGivenName from '../../../utilities/name.given.ts'
 
 const getGender = (
   relation: string,
@@ -39,15 +40,19 @@ const getKin = async (
   const oldCountry = getOldCountry()
 
   for await (const sailor of requesters) {
-    const men = ['son', 'father']
-    const women = ['daughter', 'mother', 'wife']
-    const relationship = selectRandomElement([...men, ...women, 'lover'])
+    const maleKin = ['son', 'father']
+    const femaleKin = ['daughter', 'mother', 'wife']
+    const related = [...maleKin, ...femaleKin]
+    const relationship = selectRandomElement([...maleKin, ...femaleKin, 'lover'])
     const nationality = getNationality(relationship, sailor, empire)
-    const gender = getGender(relationship, men, women)
-    const name = await generatePersonalName(nationality, gender)
+    const gender = getGender(relationship, maleKin, femaleKin)
+    const fullName = await generatePersonalName(nationality, gender)
+    const name = related.includes(relationship) ? getGivenName(fullName) : fullName
 
     const local = selectRandomElement([true, false])
-    const location = local ? await drawSettlement(empire) : oldCountry[nationality]
+    const location = local
+      ? await drawSettlement(empire)
+      : oldCountry[nationality.toLowerCase()]
 
     kin.push({ name, relationship, location, sailor })
   }
