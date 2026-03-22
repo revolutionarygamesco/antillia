07e495b5d825931a7e3366360728f665fa8c12df
Jason Godesky <jefgodesky@gmail.com>
2026-03-22 18:16:13 -0400

    Add new method: loadSugarCargo

---
+++ src/scripts/sails/load-sugar.ts
@@ -0,0 +1,17 @@
+import loadCargo from './load-cargo.ts'
+import stockArray from '../random/stock.ts'
+import { UUIDS } from '../settings.ts'
+
+const loadSugarCargo = async (
+  ship: Actor,
+  ratio: number
+): Promise<void> => {
+  const cargo = stockArray([
+    { n: 3, item: UUIDS.SUGAR_CARGO },
+    { n: 1, item: UUIDS.MOLASSES_CARGO }
+  ])
+
+  await loadCargo(ship, ratio, ...cargo)
+}
+
+export default loadSugarCargo
